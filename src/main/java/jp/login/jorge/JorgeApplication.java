14243ae609de46be68c14b0cd152d19d623d93d2
Jorge <jrge435@gmail.com>
2025-07-17 10:41:59 -0500

    feat : initial project Login-backend

---
+++ src/main/java/jp/login/jorge/JorgeApplication.java
@@ -0,0 +1,13 @@
+package jp.login.jorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JorgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JorgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/login/jorge/JorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.login.jorge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JorgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
