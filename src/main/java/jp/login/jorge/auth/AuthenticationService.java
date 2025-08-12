679bdac4af63e2122fb1cdf7971791b0e9b543ec
Jorge <jrge435@gmail.com>
2025-08-11 22:15:20 -0500

    [FEAT] new changes

--- src/main/java/jp/login/jorge/auth/AuthenticationService.java
+++ src/main/java/jp/login/jorge/auth/AuthenticationService.java
@@ -1,8 +1,36 @@
 package jp.login.jorge.auth;
 
 
+import jp.login.jorge.enitty.Rol;
+import jp.login.jorge.repository.RolRepository;
+import jp.login.jorge.repository.UserRepository;
+import jp.login.jorge.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final RolRepository rolRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest requets){
+        Rol rol = rolRepository.findByName("User");
+        if(rol == null){
+            rol = new Rol();
+            rol.setName("User");
+            rol = rolRepository.save(rol);
+        }
+    }
+
+
 }
--- src/main/java/jp/login/jorge/repository/RolRepository.java
+++ src/main/java/jp/login/jorge/repository/RolRepository.java
@@ -1,11 +1,12 @@
 package jp.login.jorge.repository;
 
+import jp.login.jorge.enitty.Rol;
 import jp.login.jorge.enitty.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RolRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
-}
+public interface RolRepository extends JpaRepository<Rol, Long> {
+
+    Rol findByName(String name);
+};
